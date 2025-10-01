c0cc12e0232227ae75d4214b98d7c6d9b2dbd2ba
Ashpaca <ashley.ashley.harrison@gmail.com>
2025-10-01 11:23:40 -0700

    changed player movement and controls

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED : float = 500.0
+const JUMP_VELOCITY : float = -600.0
+const GRAVITY_MULTIPLIER : float = 3
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y < 0 and Input.is_action_pressed("move_jump"):
+			velocity += get_gravity() * delta 
+		else:
+			velocity += get_gravity() * delta * GRAVITY_MULTIPLIER 
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction : float = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
