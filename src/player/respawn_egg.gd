bf3b9ccf3da87f65adce0b2adc8708b5ad8ff9fe
Ashpaca <ashley.ashley.harrison@gmail.com>
2025-10-03 22:02:59 -0700

    can stand on egg now

--- src/player/respawn_egg.gd
+++ src/player/respawn_egg.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
+const PLAYER_AND_GROUND_LAYER : int  = 3
+const JUST_GROUND_LAYER : int = 2
+
 func _physics_process(delta: float) -> void:
 	if not GameState.is_in_game(): queue_free()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		collision_layer = JUST_GROUND_LAYER
+		move_and_slide()
+		return
+	
+	if GameState.player_location.y < global_position.y:
+		collision_layer = PLAYER_AND_GROUND_LAYER
+	else:
+		collision_layer = JUST_GROUND_LAYER
+	
 	move_and_slide()
