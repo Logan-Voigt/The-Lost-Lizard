816b12c12c852dd565db8c6e7afe795abf9be3d0
Shadow <logan2006a@gmail.com>
2025-10-05 06:42:25 -0600

    addded credits end screen

--- src/menus/win_screen.gd
+++ src/menus/win_screen.gd
@@ -3,8 +3,12 @@ extends Node2D
 @onready var win_sound: AudioStreamPlayer2D = $win_sound
 
 var first_time_winning : bool
+var timer : int = 0
+@onready var main_menu_btn : Button = $"Back to main menu"
+@onready var main_btn_timer : Timer = $Timer
 
 func _ready() -> void:
+	main_menu_btn.visible = false
 	visible = false
 	first_time_winning = true
 
@@ -14,3 +18,11 @@ func _process(_delta: float) -> void:
 	if visible and first_time_winning:
 		first_time_winning = false
 		win_sound.play()
+		main_btn_timer.start()
+
+func _on_timer_timeout() -> void:
+	main_menu_btn.visible = true # Replace with function body.
+
+
+func _on_back_to_main_menu_pressed() -> void:
+	GameState.change_state(GameState.START_SCREEN)  # Replace with function body.
