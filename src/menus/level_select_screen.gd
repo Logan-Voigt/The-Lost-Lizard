44aa2465748bce5d2fc790e420d24a9e6aae16cb
Ashpaca <ashley.ashley.harrison@gmail.com>
2025-10-04 22:02:16 -0700

    level select works

--- src/menus/level_select_screen.gd
+++ src/menus/level_select_screen.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+func _on_level_button_pressed(level_number : int) -> void:
+	EventBus.start_level.emit(level_number)
+
+
+func _process(_delta: float) -> void:
+	visible = GameState.current_state == GameState.LEVEL_SELECT
+
+
+func _ready() -> void:
+	for i in range(get_child_count()):
+		if get_child(i) is Button:
+			var level_button : Button = get_child(i)
+			level_button.pressed.connect(_on_level_button_pressed.bind(i))
